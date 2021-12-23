614b11b10e17ca342895abd2b34f3a843b06d583
Fearless <489368600@qq.com>
2021-12-23 16:41:29 +0800

    动态扩容

--- 02-动态数组/src/com/zc/ArrayList.java
+++ 02-动态数组/src/com/zc/ArrayList.java
@@ -11,18 +11,18 @@ public class ArrayList {
 	private int[] elements;
 
 	// 默认容量 大小为10
-	private static final int DEFAULT_CAPATICY = 10;
+	private static final int DEFAULT_CAPACITY = 2;
 	private static final int ELEMENT_NOT_FOUND = -1;
 	
 	// 有参构造函数
-	public ArrayList(int capaticy) {
-		capaticy = (capaticy < DEFAULT_CAPATICY) ? DEFAULT_CAPATICY : capaticy;
-		elements = new int[capaticy];
+	public ArrayList(int capacity) {
+		capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity; 
+		elements = new int[capacity];
 	}
 	
 	// 无参构造函数
 	public ArrayList() {
-		this(DEFAULT_CAPATICY);
+		this(DEFAULT_CAPACITY);
 	}
 	
 	// 清除所有元素
@@ -47,7 +47,11 @@ public class ArrayList {
 	
 	// 添加元素到尾部
 	public void add(int element) {
-		
+//		elements[size] = element; // 添加数据
+//		size++;
+		// 等同于
+//		elements[size++] = element;
+		add(size, element); // 直接将元素添加到最后面 size 位置
 	}
 	
 	// 获取index位置的元素
@@ -71,14 +75,37 @@ public class ArrayList {
 		return old;
 	}
 	
-	// 在index位置插入一个元素
+	// 在index位置插入一个元素，从index后往后移动
 	public void add(int index, int element) {
+		if (index < 0 || index > size)	{ // 这里的判断要注意
+			// 抛出异常
+			throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+		};
 		
+		// 判断要不要扩容
+		ensureCapacity(size + 1);
+		
+		for (int i = size - 1; i >= index; i--) {
+			elements[i + 1] = elements[i];
+		}
+		elements[index] = element;
+		size++;
 	}
 	
 	// 删除index位置的元素
 	public int remove(int index) {
-		return 0;
+		if (index < 0 || index >= size)	{
+			// 抛出异常
+			throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+		};
+
+		// 先记录之前的元素
+		int old = elements[index];
+		for (int i = index+1; i <= size-1; i++) {
+			elements[i - 1] = elements[i];
+		}
+		size--;
+		return old;
 	}
 	
 	// 查看元素的索引
@@ -87,6 +114,44 @@ public class ArrayList {
 			if (elements[i] == element) return i;
 		}
 		return ELEMENT_NOT_FOUND; // -1
+	}
+	
+	// 扩容(保证要有capacity)的容量
+	public void ensureCapacity(int capacity) {
+		int oldCapacity = elements.length;
+		if (oldCapacity >= capacity) return; // 此处不需要扩容
+		// 右移动 1, oldCapacity >> 1 相当于 oldCapacity / 2
+		// 新容量 是 旧容量的 1.5倍
+		int newCapaciy = oldCapacity + (oldCapacity >> 1);
+		int[] newElements = new int[newCapaciy];
+		// 将之前elements的内容移动到 newElements 中
+		for (int i = 0; i < size; i++) {
+			newElements[i] = elements[i];
+		}
+		elements = newElements;
+		
+		System.out.println(oldCapacity + "扩容为" + newCapaciy);
+	}
+	
+	// 打印
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		// Java中字符串拼接用 StringBuilder
+		StringBuilder string = new StringBuilder();
+		string.append("size=").append(size).append("，[");
+		
+		for (int i = 0; i < size; i++) {
+//			if (i != 0) {
+//				string.append("，");
+//			}
+			string.append(elements[i]);
+			if (i != size - 1) {
+				string.append("，");
+			}
+		}
+		string.append("]");
+		return string.toString();
 	}
 	
 }
--- 02-动态数组/src/com/zc/Main.java
+++ 02-动态数组/src/com/zc/Main.java
@@ -4,12 +4,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		int array[] = new int[] {1, 2, 3, 4, 5};
 
+		// new是向堆空间申请内存
 		ArrayList list = new ArrayList();
-		list.get(0);
+		list.add(123);
+		list.add(456);
+		list.add(789);
+		list.add(111);
+		list.add(222);
+		list.add(333);	
+		list.add(444);		
+		list.add(555);		
+		list.add(666);		
+		list.add(777);		
+		list.add(888);		
+
+
+		System.out.println(list.toString());
+//		list.remove(2);
+//		list.add(4, 999);
+//		System.out.println(list.toString());
 		
 	}
 
-}
+} 
