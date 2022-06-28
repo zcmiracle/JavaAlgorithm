c9eb675cc17f8209efb6a2b71b5ae7c2e32f288d
Fearless <489368600@qq.com>
2022-06-28 15:32:58 +0800

    插入排序优化-二分搜索

--- 00-排序/src/com/zc/sort/cmp/InsertionSort1.java
+++ 00-排序/src/com/zc/sort/cmp/InsertionSort1.java
@@ -9,11 +9,11 @@ public class InsertionSort1<T extends Comparable<T>> extends Sort<T> {
 	@Override
 	protected void sort() {
 		// TODO Auto-generated method stub
-//		9 8 7 6 5 4 3 
-//		8 9 7 6 5 4 3 
-//		8 7 9 6 5 4 3  current--
-//		7 8 9 6 5 4 3 
-		
+//		9 8 7 6 5  
+//		8 9 7 6 5  
+//		8 7 9 6 5  current--
+//		7 8 9 6 5 
+
 		for (int begin = 1; begin < array.length; begin++) {
 			int current = begin;
 			while (current > 0 && cmp(current, current-1) < 0) {
@@ -21,5 +21,6 @@ public class InsertionSort1<T extends Comparable<T>> extends Sort<T> {
 				current--;
 			}
 		}
+		
 	}
 }
--- 00-排序/src/com/zc/sort/cmp/InsertionSort2.java
+++ 00-排序/src/com/zc/sort/cmp/InsertionSort2.java
@@ -4,20 +4,24 @@ import com.zc.sort.Sort;
 
 public class InsertionSort2<T extends Comparable<T>> extends Sort<T> {
 
+	/**
+	 * 1、将待插入的元素备份
+	 * 2、头部有序数据中比待插入元素大的，都朝尾部方向挪动1个位置
+	 * 2、将待插入元素放到最终的合适位置
+	 */
 	@Override
 	protected void sort() {
 		// TODO Auto-generated method stub
-		
-//		9 8 7 6 5 4 3
-//		8 9 7 6 5 4 3  array[current] = array[current-1];
-//		8 7 9 6 5 4 3  
-//		7 8 9 6 5 4 3 
-		
+//		9 8 7 6 5 
+//		8 9 7 6 5   array[current] = array[current-1];
+//		8 7 9 6 5   
+//		7 8 9 6 5 
+		// 【交换】 - 【挪动】
 		for (int begin = 1; begin < array.length; begin++) {
 			int current = begin;
-			T value = array[current];
+			T value = array[current]; // 将待插入的元素备份
 			while (current > 0 && cmp(value, array[current-1]) < 0) {
-				array[current] = array[current-1];
+				array[current] = array[current-1]; // 挪动位置
 				current--;
 			}
 			array[current] = value;
